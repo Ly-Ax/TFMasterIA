5c15c84f7a6575ff07d4d58a82f7d627d098b248
Alex Castro Gumiel <alex.castro.gumiel@gmail.com>
2023-08-11 14:17:21 -0400

    update requirements

--- src/data_main.py
+++ src/data_main.py
@@ -1,18 +1,26 @@
+"""Example module"""
 import pandas as pd
 import os
 import yaml
 
-class DataLoad():
+
+class DataLoad:
+    """Example class"""
+
     def __init__(self, config):
+        """Example function"""
         self.path = os.getcwd()
 
-        with open(config, 'r') as yaml_file:
+        with open(config, "r") as yaml_file:
             self.config = yaml.safe_load(yaml_file)
 
     def get_df(self):
-        return pd.read_csv(self.path + self.config['data']['data_path'], low_memory=False)
+        """Example function"""
+        df = pd.read_csv(self.path + self.config["data"]["data_path"], low_memory=False)
+        return df
 
-if __name__ == '__main__':
-    cnf_data = DataLoad('config.yaml')
+
+if __name__ == "__main__":
+    cnf_data = DataLoad("config.yaml")
     df = cnf_data.get_df()
     print(df.sample(3))
